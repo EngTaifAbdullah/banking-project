98009f9c72dc13b40f0b2cf274229bd2574333a9
taif <eng.taifabdullah@gmail.com>
2025-09-22 15:25:17 +0300

    Add setUp in TEST account

--- test/test_account.py
+++ test/test_account.py
@@ -8,28 +8,41 @@ from bank.account import Account
 
 class TestAccount(unittest.TestCase):
 
-# ---------------- Deposit Test  ----------------
+    def setUp(self):
 
-    def test_deposit(self):
-        acc = Account("10001", "checking account" , 600)
-        acc.deposit(300)
+        self.acc = Account("10001", "checking", 1000)
 
-        self.assertEqual(acc.get_balance(), 900)
-
-# ---------------- Withdraw Test  ----------------
-
-    def test_withdraw_normal(self):
-        acc = Account("10006", "savings account", 2000)
-        acc.withdraw(500)
-
-        self.assertEqual(acc.get_balance(), 1500)
-
-# ------------ Withdraw Overdraft Test  ------------
+# -------------------------- Deposit Test  --------------------------
 
 
+    def test_deposit_valid(self):
+
+        self.acc.deposit(500)
+        self.assertEqual(self.acc.get_balance(), 1500)
 
 
-# --------------------------------------------------
+# -------------------------- Withdraw Test  --------------------------
+
+
+    def test_withdraw_valid(self):
+        
+        self.acc.withdraw(900)
+        self.assertEqual(self.acc.get_balance(), 100)
+
+# ---------------------- Withdraw Overdraft Test  ----------------------
+
+
+
+
+
+
+# ------------------ Withdraw Deactivation Test ------------------------
+
+
+
+
+
+# -----------------------------------------------------------------------
 
 if __name__ == "__main__":
     unittest.main()
