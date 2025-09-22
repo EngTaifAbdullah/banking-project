cef6b7bf08ed4267046c71e82bb14db1777762e3
taif <eng.taifabdullah@gmail.com>
2025-09-22 12:08:04 +0300

    Create Account Class

--- bank/account.py
+++ bank/account.py
@@ -1,11 +1,20 @@
 #--------------------------------------- INCLOUD INFORMATIONS ABOUT USER ACCOUNT ---------------------------------------
 
 class Account:
+    def __init__(self, account_id, account_type, balance=0):
+     
+        self.account_id = account_id
+        self.account_type = account_type
+        self.balance = balance
 
-# account_id
-# balance
-# account_type
+    def deposit(self, amount):
+
+        if amount > 0:
+            self.balance += amount
+        
+        else:
+            raise ValueError("The deposited amount MUST be greater than zero!")
+        return self.balance
 
 
 
-# status
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #--------------------------------------- INCLOUD INTARFACE ---------------------------------------
+
+
+
+    print("1. Add Customer")
+    print("2. Login")
+    print("3. Deposit")
+    print("4. Withdraw")
+    print("5. Transfer")
+    print("6. Exit")
