50b922056448581c3b2df372fe1948c19d044b50
taif <eng.taifabdullah@gmail.com>
2025-09-22 12:47:04 +0300

    Add Deposit TEST

--- test/test_account.py
+++ test/test_account.py
@@ -1,1 +1,18 @@
 #--------------------------------------- INCLOUD THE TEST FOR ACCOUNT  ---------------------------------------
+
+
+import unittest
+
+from bank.account import Account
+
+class TestAccount(unittest.TestCase):
+
+# ----------- Deposit Test  -----------
+
+    def test_deposit(self):
+        acc = Account("10001", "checking" , 600)
+        acc.deposit(300)
+        self.assertEqual(acc.get_balance(), 900)
+
+if __name__ == "__main__":
+    unittest.main()
