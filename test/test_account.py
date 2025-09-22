2729dcaaac832ca34ef6af62d3c02f96c54f8ea4
taif <eng.taifabdullah@gmail.com>
2025-09-22 09:39:25 +0300

    Monday Standup

--- test/test_account.py
+++ test/test_account.py
@@ -2,5 +2,9 @@ import unittest
 
 class TestSavingsAccount (unittest.TestCase):
     def test_deposit_into_savings (self) :
+
         customer = Customer ('1001', 'Taif', 'Abdullah')
         account = Account (checking=0, savings=0)
+        bank = Bank            
+        # here information 
+        
